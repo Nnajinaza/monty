9d4e17a112b946390babda2ba213b2b0b9875171
Nnajinaza <nnajinaza@gmail.com>
2022-07-17 14:57:21 +0000

    TASK 0

--- push.c
+++ push.c
@@ -9,7 +9,7 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	char *new_args = global.args;
 
-	if (is_digit(new_args) == 0 || new_args == NULL)
+	if (is_digit(new_args) == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
@@ -25,7 +25,6 @@ void push(stack_t **stack, unsigned int line_number)
 		if(!tail_stack(stack, atoi(global.args)))
 			exit(EXIT_FAILURE);
 	}
-	line_number++;
 }
 
 /**
