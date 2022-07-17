4ae5fdc47f896911150fa21a6116f8acea08ce86
Nnajinaza <nnajinaza@gmail.com>
2022-07-17 15:37:56 +0000

    file change

--- monty.c
+++ monty.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(argv[1], "r");
-	if (!fd)
+	if (fd == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
